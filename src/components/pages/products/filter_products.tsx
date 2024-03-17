a38626d6b3d392079d79936a912abcce0e4d8ea8
quanber123 <quantm103@gmail.com>
2024-03-18 01:40:43 +0700

    [fix] layout

--- src/components/pages/products/filter_products.tsx
+++ src/components/pages/products/filter_products.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const FilterProducts = () => {
   return (
     <section className='grid lg:grid-cols-5 grid-cols-1 gap-[20px]'>
--- src/views/(logged-in)/ProductsViews.tsx
+++ src/views/(logged-in)/ProductsViews.tsx
@@ -1,33 +1,12 @@
-import Table from '@/components/(ui)/table/table';
 import FilterProducts from '@/components/pages/products/filter_products';
-import { useGetProductsQuery } from '@/services/redux/features/products';
-import { useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 const ProductsViews = () => {
   const { t } = useTranslation('translation');
-  const [page, setPage] = useState(1);
-  const [search, setSearch] = useState('');
-  const [category, setCategory] = useState('');
-  const [price, setPrice] = useState('');
-  const { data: productsData, isSuccess: isSuccessProducts } =
-    useGetProductsQuery(
-      `page=${page}&category=${category}&sort=${price}&search=${search}`
-    );
-  const renderedProducts = useMemo(() => {}, [isSuccessProducts, productsData]);
-  const handleFilter = (s: string, c: string, p: string) => {
-    setSearch(s);
-    setCategory(c);
-    setPrice(p);
-  };
-  const handleChangePage = (p: number) => {
-    setPage(p);
-  };
   return (
     <>
       <h2 className='text-lg font-bold'>{t('products')}</h2>
       <FilterProducts />
-      {/* {isSuccessProducts && productsData.products.length > 0 && <Table tHeader={[`${t('product_name')}`, `${t('category')}`, `${t('price')}`, `${t('sale_price')}`, `${t('stock')}`, `${t('status')}`, `${t('view')}`, `${t('published')}`, `${t('action')}`]} renderedData={} handleChangePage={handleChangePage} totalPage={productsData.totalPage}/>} */}
     </>
   );
 };
