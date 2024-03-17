a557cafdd2c42a87cf72ea1b01d0f2bd787aab58
quanber123 <quantm103@gmail.com>
2024-03-18 03:01:08 +0700

    fix

--- src/components/(ui)/not-found-item/not_found_item.tsx
+++ src/components/(ui)/not-found-item/not_found_item.tsx
@@ -5,7 +5,7 @@ type Props = {
 const NotFoundItem: React.FC<Props> = ({ img, message }) => {
   return (
     <section className='w-full rounded-lg border border-lightGray dark:border-darkGray overflow-x-auto p-8 flex flex-col items-center gap-[20px]'>
-      {img}
+      <div dangerouslySetInnerHTML={{ __html: img }}></div>
       <p className='font-bold text-darkGray dark:text-gray'>{message}</p>
     </section>
   );
