6c650682db80d472842e11f39b9cc764f7afeccd
quanber123 <quantm103@gmail.com>
2024-03-22 12:53:48 +0700

    [fix] protected route

--- src/auth/ProtectedRoute.tsx
+++ src/auth/ProtectedRoute.tsx
@@ -1,17 +1,19 @@
-import { PropsWithChildren, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { PropsWithChildren } from 'react';
+import { Navigate } from 'react-router-dom';
 
 import { useAuth } from '@/context/AuthProvider';
 type ProtectedRouteProps = PropsWithChildren;
 
 export default function ProtectedRoute({ children }: ProtectedRouteProps) {
   const [user] = useAuth();
-  const navigate = useNavigate();
-  useEffect(() => {
-    if (user === null) {
-      navigate('/login', { replace: true });
-    }
-  }, [navigate, user]);
-
+  // const navigate = useNavigate();
+  // useEffect(() => {
+  //   if (user === null) {
+  //     navigate('/login', { replace: true });
+  //   }
+  // }, [navigate, user]);
+  if (user === null) {
+    return <Navigate to={'/login'} replace />;
+  }
   return children;
 }
